9e05519380cc0c36323d7603344d1dfcd7c9eaba
LAPTOP-PSD7H2LB <lebaophuc15022k3@gmail.com>
2024-04-25 01:28:24 +0700

    commit1_TaoDoanMaJava

---
+++ src/main/java/com/myprograming/myprograming/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.myprograming.myprograming;
+
+/**
+ *
+ * @author Admin
+ */
+public class HelloWorld {
+    public static String HelloWorldMessage(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/myprograming/myprograming/Myprograming.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.myprograming.myprograming;
+
+/**
+ *
+ * @author Admin
+ */
+public class Myprograming {
+
+    public static void main(String[] args) {
+        System.out.println(HelloWorld.HelloWorldMessage());
+    }
+}
